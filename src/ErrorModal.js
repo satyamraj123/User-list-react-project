bc576e4a28db928a3e3e94155222c9e87508ef10
satyamraj <satyamraj-creator@users.noreply.github.com>
2022-03-17 00:15:27 +0530

    portal

--- src/ErrorModal.js
+++ src/ErrorModal.js
@@ -1,24 +1,49 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Button from "./Button";
 import Card from "./Card";
 import styles from "./ErrorModal.module.css";
 
+const BackDrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onClick} />;
+};
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onClick}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+//portal is used to transfer one component to any desired location in html tree
+//here backdrop is tranfered inside a div whose id is root-backdrop
+//here modaloverlay is tranfered inside a div whose id is root-overlay
+//even tho they are deep nested inside the tree
+//but they will jump off to any desired location
+//modal overlays should be on top of tree, thats why portal is useful here
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onClick}/>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onClick}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <BackDrop onClick={props.onClick}></BackDrop>,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onClick={props.onClick}
+        ></ModalOverlay>,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
